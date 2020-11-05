3dc8d3566d5c767fd93bfd4777252c8ba0f5bbe4
Srinivas Pobba <ravikiran.ps@gmail.com>
2020-11-05 16:14:24 -0500

    Initial Commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ExternalAPICons.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ExternalAPICons.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Datum> datumList = null;
+            using (var client = new HttpClient()) {
+                client.BaseAddress = new Uri("https://gorest.co.in");
+                HttpResponseMessage response = client.GetAsync("/public-api/users").Result;
+                string jsonObj = response.Content.ReadAsStringAsync().Result;
+                if (response.IsSuccessStatusCode) {
+                    var deserialisedRootObj = JsonConvert.DeserializeObject<Root>(jsonObj);
+                    datumList = deserialisedRootObj.data;
+                }
+            }
+            return View(datumList);
+        }
+    }
+}
+
+
+
+
+
+
+// user =JsonConvert.DeserializeObject<List<UsersModel>>(stringData);
+
+//if (result.IsSuccessStatusCode)
+//{
+//    var readJob = result.Content.ReadAsAsync<IList<UsersModel>>();
+//    readJob.Wait();
+//    user = readJob.Result;
+//}
+//else
+//{
+//    user = Enumerable.Empty<UsersModel>();
+//    ModelState.AddModelError(string.Empty, "Server error");
+//}
+//client.BaseAddress = new Uri("https://gorest.co.in/public-api/users");
+
+//var responseTask = client.GetAsync("users");
+//responseTask.Wait();
+//var result = responseTask.Result;
---
+++ Models/UsersModel.cs
@@ -0,0 +1,98 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExternalAPICons.Models
+{
+
+    /*    public class JsonRoot
+        {
+            public string code { get; set; }
+            public MetaCust Metacust { get; set; }
+            public List<DataCust> Datacust { get; set; }
+        }
+
+        public class MetaCust
+        {
+
+            public string UserId { get; set; }
+
+            public string UserName { get; set; }
+
+            public string Gender { get; set; }
+
+            public string Status { get; set; }
+        }
+
+        public class PaginationCust
+        {
+            public string UserId { get; set; }
+
+            public string UserName { get; set; }
+
+            public string Gender { get; set; }
+
+            public string Status { get; set; }
+        }
+
+        public class DataCust
+        {
+
+            public string UserId { get; set; }
+
+            public string UserName { get; set; }
+
+            public string Gender { get; set; }
+
+            public string Status { get; set; }
+        }
+        public class UsersModel
+        {
+
+            public string UserId { get; set; }
+
+            public string UserName { get; set; }
+
+            public string Gender { get; set; }
+
+            public string Status { get; set; }
+        }
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+ 
+ */
+
+        public class Root
+        {
+            public int code { get; set; }
+            public Meta meta { get; set; }
+            public List<Datum> data { get; set; }
+        }
+
+        public class Pagination
+        {
+            public int total { get; set; }
+            public int pages { get; set; }
+            public int page { get; set; }
+            public int limit { get; set; }
+        }
+
+        public class Meta
+        {
+            public Pagination pagination { get; set; }
+        }
+
+        public class Datum
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public string email { get; set; }
+            public string gender { get; set; }
+            public string status { get; set; }
+            public DateTime created_at { get; set; }
+            public DateTime updated_at { get; set; }
+        }
+
+}
+
